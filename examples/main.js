aac5a4f18fdfb892c2ab3fa781b4b61223d265fb
fiveroom <lkzipzxc@163.com>
2021-06-01 17:05:34 +0800

    初始化

--- examples/main.js
+++ examples/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import coldUi from '../packages'
+
 Vue.config.productionTip = false
+
+Vue.use(coldUi);
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
+
+
